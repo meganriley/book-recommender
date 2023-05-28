1fee557b8dc23fea5877686cfb36e7f45b5e9591
Meg <meganriley806@gmail.com>
2023-05-28 16:31:32 -0500

    version 1.0

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+class Footer extends React.Component {
+    render() {
+        return(
+            <footer>Made with love by a fellow book-lover</footer>
+        )
+    }    
+}
+
+export default Footer
---
+++ src/config.js
@@ -0,0 +1,5 @@
+var config = {
+    MY_KEY : '89Ikp7GhsfsEMChsHDZpbuHKtNXC3OsY'
+}
+
+export default config
